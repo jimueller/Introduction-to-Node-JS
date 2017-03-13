6900010ce5ea232f4272b41137b8348ea4a09ccd
Jim Mueller <jim@jimueller.com>
2017-03-12 21:17:33 -0500

    HOL3 - Rest API completed

--- hol3.js
+++ hol3.js
@@ -1,35 +1,50 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+// to support JSON-encoded request bodies
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 var products = [
-    {id:1, name:"Twix", price:2.9},
-    {id:2, name:"Snickers", price:2.5},
-    {id:3, name:"Daim", price:3.2}
+    {id: 1, name: "Twix", price: 2.9},
+    {id: 2, name: "Snickers", price: 2.5},
+    {id: 3, name: "Daim", price: 3.2}
 ];
- 
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.send('Hello World')
 });
 
-app.get('/products', function(req, res){
-    if(req.query.id){
-        var match = null;
+app.get('/products', function (req, res) {
+    if (req.query.id) {
+        var match = products.filter(function (product) {
+            return (product.id == req.query.id);
+        });
 
-        for(var i = 0,len = products.length; i < len; i++){
-            if(String(products[i].id) === String(req.query.id)){
-                match = products[i];
-                break;
-            }
-        }
-
-        if(match){
+        if (match.length > 0) {
             res.send(match);
         } else {
-            res.send("Ain't no product with that id.");
+            res.sendStatus(404);
         }
     } else {
         res.send(products);
     }
 });
- 
+
+app.post('/products', function (req, res) {
+    var product = req.body;
+    products.push(product);
+    res.send(products);
+});
+
+app.delete('/products', function (req, res) {
+    var id = req.query.id;
+    products = products.filter(function (product) {
+       return product.id != id;
+    });
+
+    res.send(products);
+});
+
 app.listen(3000);
